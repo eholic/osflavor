97c3760c7b6e42d39f81437723470ad1e441f508
T. Imanishi <task@mxxiv.net>
2020-11-14 20:19:50 +0900

    [fix] test

--- tests/osflavor.fish
+++ tests/osflavor.fish
@@ -4,7 +4,7 @@ function _decolor -a text
     string replace --all --regex '\e(\[[\d;]*|\(B\e\[)m' '' "$text"
 end
 
-@test "default" (osflavor set DEFAULT; _decolor (__osflavor)) = "❯❮❯"
+@test "OTHERS" (osflavor set OTHERS; _decolor (__osflavor)) = "❯❮❯"
 @test "overwrite default" (osflavor set DEFAULT; set -g __osflavor_dist_DEFAULT ">"; _decolor (__osflavor)) = ">"
 @test "microsoft" (osflavor set microsoft; _decolor (__osflavor)) = "■■■■"
 @test "darwin" (osflavor set darwin; _decolor (__osflavor)) = "||||||"
